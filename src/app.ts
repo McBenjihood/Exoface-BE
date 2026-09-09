e48512b00f9bf023f08e9faf47f99a4c43a429c1
Benjamin Gerresheim <benjaminmikagerresheim@gmail.com>
2026-09-09 12:53:04 +0200

    - switched from js to ts

--- src/app.ts
+++ src/app.ts
@@ -6,19 +6,19 @@ const path = require('path');
 const app = express();
 
 const swaggerDocument = JSON.parse(
-  fs.readFileSync(path.join(__dirname, './exoscale-api-openapi-source.json'), 'utf8')
+    fs.readFileSync(path.join('./exoscale-api-openapi-source.json'), 'utf8')
 );
 
 swaggerDocument.servers = [
-  {
-    url: 'http://localhost:4010',
-    description: 'Local Prism Mock Server'
-  }
+    {
+        url: 'http://localhost:4010',
+        description: 'Local Prism Mock Server'
+    }
 ];
 
 app.use('/api/exoscale/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(3000, () => {
-  console.log('Server läuft auf Port 3000');
+    console.log('Server läuft auf Port 3000');
 });
 
